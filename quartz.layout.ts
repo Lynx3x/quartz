5febc63af37bb7e1612879b9277ff62b0a45f8cb
Lynx3x <Larynx_y@hotmail.com>
2024-08-14 00:47:31 +0800

    feat: 接入giscus评论

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+  Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'Lynx3x/quartz',
+      // from data-repo-id
+      repoId: 'R_kgDOMig9NQ',
+      // from data-category
+      category: 'Announcements',
+      // from data-category-id
+      categoryId: 'DIC_kwDOMig9Nc4ChloS',
+    }
+  }),
+],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
